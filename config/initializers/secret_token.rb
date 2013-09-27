4c528b8c5c105afca6155acc71fcd48aeb11f30b
dimension64 <64dimensions@gmail.com>
2013-09-27 15:26:03 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Openshift::Application.config.secret_key_base = '5ea05fc98bac4eff15e44f4d3976a78914324f218acaab6ed3594c2344e7fbb0836d6189761b1e2545b382d0870132b89ebff396fbe005796dfe3316d3425cf0'
